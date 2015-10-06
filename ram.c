5c1f13f7609c81346791dd567e6bc88ce02ee497
tovarchristian21 <tovarchristian21@gmail.com>
2015-10-06 10:28:21 -0500

    Se implementaron todas la funciones de carga

--- ram.c
+++ ram.c
@@ -55,10 +55,46 @@ void pop(uint32_t *registro,uint8_t *SRAM,uint8_t *registers_list)
     }
     registro[13]=registro[13]+4*bitcount;
 }
+
 void LDR(uint32_t *Rt,uint32_t Rn,uint32_t Rm,uint8_t *SRAM)
 {
-    uint32_t direccion=Rn+(Rm<<2);
+    uint32_t direccion=Rn+Rm;
     *Rt=(uint32_t)(SRAM[direccion]<<24)+(uint32_t)(SRAM[direccion+1]<<16)+(uint32_t)(SRAM[direccion+2]<<8)+SRAM[direccion+3];
 
-  //Rt recibe el valor de los 4 bytes superioires en memoria
+  //Rt recibe el valor de los 4 bytes superiores en memoria
+}
+
+void LDRB(uint32_t *Rt,uint32_t Rn,uint32_t Rm,uint8_t *SRAM)
+{
+    uint32_t direccion=Rn+Rm;
+    *Rt=(uint32_t)(SRAM[direccion]<<24);
+
+
+}
+
+void LDRH(uint32_t *Rt,uint32_t Rn,uint32_t Rm,uint8_t *SRAM)
+{
+    uint32_t direccion=Rn+Rm;
+    *Rt=(uint32_t)(SRAM[direccion]<<24)+(uint32_t)(SRAM[direccion+1]<<16) ;
+
+
+
+}
+
+void LDRSB(uint32_t *Rt,uint32_t Rn,uint32_t Rm,uint8_t *SRAM)
+{
+    uint32_t direccion=Rn+Rm;
+    *Rt=(uint32_t)(SRAM[direccion]<<24) ;
+    *Rt=(int32_t)(*Rt) ;
+
+
+}
+
+void LDRSH(uint32_t *Rt,uint32_t Rn,uint32_t Rm,uint8_t *SRAM)
+{
+    uint32_t direccion=Rn+Rm;
+    *Rt=(uint32_t)(SRAM[direccion]<<24)+(uint32_t)(SRAM[direccion+1]<<16) ;
+    *Rt=(int32_t)(*Rt) ;
+
+
 }
